6961014b33306ed9ba8999857dd08a30021da9a9
Ilham <mi1797128@gmail.com>
2024-12-03 16:50:07 +0700

    fix users

--- database/seeders/RolePermissionSeeder.php
+++ database/seeders/RolePermissionSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class RolePermissionSeeder extends Seeder
 {
@@ -20,10 +22,40 @@ class RolePermissionSeeder extends Seeder
             'manage transaksi',
             'manage keanggotaan',
             'manage user',
+            'checkout lapangan',
+            'view lapangan bookings'
         ];
         
         foreach($permission as $perm){
-            Permission::create()
+            Permission::firstOrCreate([
+                'name' => $perm
+            ]);
         }
+
+        $customerRole = Role::firstOrCreate([
+            'name' => 'pengguna'
+        ]);
+
+
+        $customerPermissions = [
+            'checkout lapangan',
+            'view lapangan bookings'
+        ];
+
+        $customerRole->syncPermissions($customerPermissions);
+
+        $superAdminRole = Role::firstOrCreate([
+            'name' => 'super_admin'
+        ]);
+
+        $user = User::create([
+            'name' => 'Super_Admin',
+            'email' => 'admin@gmail.com',
+            'avatar' =>'image/dummyavatar.png',
+            'password' => bcrypt('admin123'),
+            
+        ]);
+
+        $user->assignRole($superAdminRole);
     }
 }
